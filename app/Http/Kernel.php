d2b2c9f4fd3e045c0308807fc3932d378cbcd284
carylfaithabellano <carylfaithabellano2000@gmail.com>
2023-01-23 22:52:06 +0800

    added route dashboard and middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'employee' => \App\Http\Middleware\EmployeeAuthenticated::class,
+        'admin' => \App\Http\Middleware\AdminAuthenticated::class,
     ];
 }
