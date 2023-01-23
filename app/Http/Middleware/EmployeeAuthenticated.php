9644c5a66ebbb316ca75d0274eb77d9d455bc7ca
carylfaithabellano <carylfaithabellano2000@gmail.com>
2023-01-23 22:45:23 +0800

    edit EmployeeAuthenticated middlware

--- app/Http/Middleware/EmployeeAuthenticated.php
+++ app/Http/Middleware/EmployeeAuthenticated.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EmployeeAuthenticated
 {
@@ -16,6 +18,22 @@ class EmployeeAuthenticated
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if( Auth::check() )
+        {
+            /** @var User $user */
+            $user = Auth::user();
+
+            // if user is not admin take him to his dashboard
+            if ( $user->hasRole('admin') ) {
+                return redirect(route('admin_dashboard'));
+            }
+
+            // allow admin to proceed with request
+            else if ( $user->hasRole('employee') ) {
+                return $next($request);
+            }
+        }
+
+        abort(403);  // permission denied error
     }
 }
