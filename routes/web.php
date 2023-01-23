8a981122ab2a9457288557b7d526072f820b1518
carylfaithabellano <carylfaithabellano2000@gmail.com>
2023-01-23 22:57:41 +0800

    added HomeController and route

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+//---------PUBLIC ROUTES-------------//
+Route::get ('/', [HomeController::class, 'index'])->name('home.index');
+
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
