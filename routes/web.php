83bd8961673802b92f5ea0304d8bc833837d6e72
carylfaithabellano <carylfaithabellano2000@gmail.com>
2023-01-24 00:11:49 +0800

    added products page, controller + contact page, controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,6 +32,8 @@ use Illuminate\Support\Facades\Route;
 Route::get ('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcement.index');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
 
 
 
