f7ee8d1c360480363ffc953cfd731579d1d326fc
carylfaithabellano <carylfaithabellano2000@gmail.com>
2023-01-23 22:40:45 +0800

    edited AdminAuthenticated middleware

--- app/Http/Middleware/AdminAuthenticated.php
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -3,7 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AdminAuthenticated
 {
@@ -16,6 +19,23 @@ class AdminAuthenticated
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if( Auth::check() )
+        {
+            /** @var User $user */
+            $user = Auth::user();
+
+            // if user is not admin take him to his dashboard
+            if ( $user->hasRole('employee') ) {
+                return redirect(route('dashboard'));
+            }
+
+            // allow admin to proceed with request
+            else if ( $user->hasRole('admin') ) {
+                return $next($request);
+            }
+        }
+
+        abort(403);  // permission denied error
     }
+    
 }
